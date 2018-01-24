7b261c0274af249215baf7051cf490b7d13e583d
Tamara <tnat93@users.noreply.github.com>
2018-01-24 15:56:50 -0600

    Add static folder
    Static folder includes:
    - CSS 
    -img
    - JS

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+/*
+Template Name: Effacy - Creative One Page Template
+Author: ThemeBite
+Author URI: http://themebite.com/
+Version: 1.0
+Developed and Designed By: ThemeBite + bits by Bryan Deloeste
+*/
